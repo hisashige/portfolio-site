acec3cb0fcb1b5632bcfb8a92db22e789615daab
hisashige <hisashige56@gmail.com>
2023-10-31 18:26:56 +0900

    chore: modify article page title

--- app/articles/layout.tsx
+++ app/articles/layout.tsx
@@ -2,7 +2,7 @@ import Hero from '@/app/_components/Hero';
 import Sheet from '@/app/_components/Sheet';
 
 export const metadata = {
-  title: '記事｜Hisashige Takahashi',
+  title: '記事｜ hisachii',
 };
 
 type Props = {
