2b193ec9cd885c95984206f2f5b7e728b48e8ad4
hisashige <hisashige56@gmail.com>
2023-11-11 17:56:13 +0900

    feat: setting for google tag manager event

--- app/_components/Footer/index.tsx
+++ app/_components/Footer/index.tsx
@@ -1,20 +1,33 @@
 import Link from 'next/link';
 import styles from './index.module.css';
 
+const FooterItems = [
+  {
+    href: '/',
+    name: 'TOP',
+  },
+  {
+    href: '/articles',
+    name: '記事一覧',
+  },
+  {
+    href: '/contact',
+    name: 'お問い合わせ',
+  },
+];
+
 export default function Footer() {
   return (
     <footer className={styles.footer}>
       <nav className={styles.nav}>
         <ul className={styles.items}>
-          <li className={styles.item}>
-            <Link href="/">TOP</Link>
-          </li>
-          <li className={styles.item}>
-            <Link href="/articles">記事一覧</Link>
-          </li>
-          <li className={styles.item}>
-            <Link href="/contact">お問い合わせ</Link>
-          </li>
+          {FooterItems.map((item, key) => (
+            <li className={styles.item}>
+              <Link href={item.href} key={key} className="footer-link">
+                {item.name}
+              </Link>
+            </li>
+          ))}
         </ul>
       </nav>
       <p>© Hisashige Takahashi. All Rights Reserved</p>
